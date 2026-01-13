22a82ebd9aee7eb58923d99592f611411dee3f06
Gweru-Tech <Gwerutechnology@gmail.com>
2026-01-13 22:52:57 +0200

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,70 @@
+┍━❑ ᴍᴏᴏɴ xᴍᴅ ᴍɪɴɪ ❑━━∙∙⊶
+┃➸╭──────────
+┃❑│▸ ʙᴏᴛɴᴀᴍᴇ: *ᴍᴏᴏɴ xᴍᴅ ᴍɪɴɪ*
+┃❑│▸ *ᴏᴡɴᴇʀ :* ᴋᴇɪᴛʜ ᴛᴇᴄʜ
+┃❑│▸ ꜱᴛᴀᴛᴜꜱ: *ᴏɴʟɪɴᴇ*
+┃❑│▸ ʀᴜɴᴛɪᴍᴇ: ${hours}h ${minutes}m ${seconds}s
+┃❑│▸ *ʜᴏꜱᴛ :* Panel
+┃❑│▸ *ᴍᴏᴅᴇ :* Public
+┃❑│▸ ᴀᴄᴛɪᴠᴇ ᴜꜱᴇʀꜱ: ${activeSockets.size}
+┃❑│▸ ᴅᴇᴠᴇʟᴏᴘᴇʀ: ᴋᴇɪᴛʜ ᴛᴇᴄʜ
+┃➸╰──────────
+┕━━━━━━━━━━━━━∙∙⊶
+
+
+┎ ❑ *𝐌𝐀𝐈𝐍 𝐌𝐄𝐍𝐔* ❑
+│▸  ${config.PREFIX}ᴀʟɪᴠᴇ
+│▸  ${config.PREFIX}ᴀɪ
+│▸  ${config.PREFIX}ꜰᴀɴᴄʏ
+│▸  ${config.PREFIX}ʟᴏɢᴏ
+│▸  ${config.PREFIX}ᴘɪɴɢ
+│▸  ${config.PREFIX}ʙɪʙʟᴇ
+┖❑
+
+┎ ❑ *M𝐄𝐃𝐈𝐀 𝐌𝐄𝐍𝐔* ❑
+│▸ ${config.PREFIX}ꜱᴏɴɢ
+│▸ ${config.PREFIX}ᴀɪɪᴍɢ
+│▸ ${config.PREFIX}ᴛɪᴋᴛᴏᴋ
+│▸ ${config.PREFIX}ꜰʙ
+│▸ ${config.PREFIX}ɪɢ
+│▸ ${config.PREFIX}ᴛꜱ
+┖❑
+
+┎ ❑ *𝐃𝐎𝐖𝐍𝐋𝐎𝐀𝐃 𝐌𝐄𝐍𝐔* ❑
+│▸ ${config.PREFIX}ᴀᴘᴋ
+│▸ ${config.PREFIX}ɢɪᴛᴄʟᴏɴᴇ
+┖❑
+
+┎ ❑ *I𝐍𝐅𝐎 𝐌𝐄𝐍𝐔* ❑
+│▸ ${config.PREFIX}ɴᴇᴡꜱ
+│▸ ${config.PREFIX}ɴᴀꜱᴀ
+│▸ ${config.PREFIX}ᴄʀɪᴄᴋᴇᴛ
+┖❑
+
+┎ ❑ *T𝐎𝐎𝐋𝐒 𝐌𝐄𝐍𝐔* ❑
+│▸ ${config.PREFIX}ᴡɪɴꜰᴏ
+│▸ ${config.PREFIX}ʙᴏᴍʙ
+│▸ ${config.PREFIX}ᴅᴇʟᴇᴛᴇᴍᴇ
+┖❑
+
+
+
+
+
+┎━❑ ${settings.botName} ❑━⋅⊶
+┃➸╭─────────
+┃❑│▸ 👤 *ᴜꜱᴇʀ :* *@${pushname}*
+┃❑│▸ 👑 *ᴏᴡɴᴇʀ :* ${settings.botOwner}
+┃❑│▸ 📦 *ᴠᴇʀꜱɪᴏɴ :* ${settings.version}
+┃❑│▸ 📚 *ᴄᴍᴅꜱ :* ${totalCommands}
+┃❑│▸ 🌍 *ᴛɪᴍᴇᴢᴏɴᴇ :* ${settings.timezone}
+┃❑│▸ 🔋 *ᴜᴘᴛɪᴍᴇ :* ${uptime}
+┃❑│▸ 💻 *ʜᴏꜱᴛ :* ${platform}
+┃❑│▸ ⚙️ *ᴍᴏᴅᴇ :* ${settings.commandMode}
+┃❑│▸ 📈 ʀᴀᴍusage: ${ramUsage.text} (${ramUsage.percentage}%)
+┃➸╰─────────
+┖━━━━━━━━━━━━⋅⊶
+${readMore}
+┎ ❑ *𝐌𝐀𝐈𝐍 𝐌𝐄𝐍𝐔* ❑
+${formatCommands(COMMAND_CATEGORIES.GENERAL)}
+┖━━━━━━━━━⋅⊶

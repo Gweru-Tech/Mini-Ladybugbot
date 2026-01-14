1f99495128e483ca61f5b22645f6bf2b77a9fb1b
Gweru-Tech <Gwerutechnology@gmail.com>
2026-01-14 04:37:33 +0200

    Update config.js

--- config.js
+++ config.js
@@ -14,9 +14,9 @@ module.exports = {
   MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://ntando:ih2T2UfoPwWrAZzJ@ladybug2017.xcgevpw.mongodb.net/?appName=Ladybug2017',
   
   // Bot behavior
-  AUTO_VIEW_STATUS: process.env.AUTO_VIEW_STATUS || 'false',
-  AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || 'false',
-  AUTO_RECORDING: process.env.AUTO_RECORDING || 'false',
+  AUTO_VIEW_STATUS: process.env.AUTO_VIEW_STATUS || 'true',
+  AUTO_LIKE_STATUS: process.env.AUTO_LIKE_STATUS || 'true',
+  AUTO_RECORDING: process.env.AUTO_RECORDING || 'true',
   AUTO_LIKE_EMOJI: parseList(process.env.AUTO_LIKE_EMOJI, ['💋', '🍬', '🫆', '💗', '🎈', '🎉', '🥳', '❤️', '🧫', '🐭']),
   PREFIX: process.env.PREFIX || '.',
   MAX_RETRIES: parseInt(process.env.MAX_RETRIES || '3', 10),
@@ -27,12 +27,12 @@ module.exports = {
   NUMBER_LIST_PATH: process.env.NUMBER_LIST_PATH || './numbers.json',
 
   // Images / UI
-  RCD_IMAGE_PATH: process.env.RCD_IMAGE_PATH || 'http://keith-cdn.vercel.app/file/hpp270.jpg',
-  CAPTION: process.env.CAPTION || '*M O O N* 𝗫 𝗠 𝗗  𝗠𝗜𝗡𝗜',
+  RCD_IMAGE_PATH: process.env.RCD_IMAGE_PATH || 'https://files.catbox.moe/7k4awc.jpeg',
+  CAPTION: process.env.CAPTION || 'ʟᴀᴅʏʙᴜɢ ʙᴏᴛ ᴍɪɴɪ',
 
   // Newsletter / channels
   NEWSLETTER_JID: (process.env.NEWSLETTER_JID || '120363417440480101@newsletter').trim(),
-  CHANNEL_LINK: process.env.CHANNEL_LINK || 'https://whatsapp.com/channel/0029VbANWX1DuMRi1VNPlB0y',
+  CHANNEL_LINK: process.env.CHANNEL_LINK || 'https://chat.whatsapp.com/LdogfpeSy9AJkvT78WZ9AD',
 
   // OTP & owner
   OTP_EXPIRY: parseInt(process.env.OTP_EXPIRY || '300000', 10), // ms
@@ -40,5 +40,5 @@ module.exports = {
 
   // Misc
   GROUP_INVITE_LINK: process.env.GROUP_INVITE_LINK || 'https://chat.whatsapp.com/Ir5dLLFsZVaEXklBsYeHSe?mode=wwt',
-  PM2_NAME: process.env.PM2_NAME || 'MOON-MINI-main'
+  PM2_NAME: process.env.PM2_NAME || 'LADYBUG-MINI-main'
 };
